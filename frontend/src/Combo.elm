e08ff3d8e3cce279f15fca2ad846de356df2aef4
Asier Elorz <asielorz@gmail.com>
2022-06-23 21:47:39 +0200

    Fixed misalignment of combos when giving them fixed sizes

--- frontend/src/Combo.elm
+++ frontend/src/Combo.elm
@@ -32,18 +32,17 @@ view_combo_content attributes args = UI.column
 
 view : List (UI.Attribute msg) -> { combo_state : a, id : id, alternatives : List a, view_alternative : (a -> UI.Element msg), message : (Msg a -> msg), currently_open_id : Maybe id } -> UI.Element msg
 view attributes args = 
-  let open = Just args.id == args.currently_open_id
-  in UI.el 
-    ( add_if 
-      open 
-      (UI.below (view_combo_content attributes { alternatives = args.alternatives, view_alternative = args.view_alternative, message = args.message })) 
-      [ UI.width UI.fill ]
-    )
-    <| Input.button 
-      (add_if open (Events.onClick <| args.message Msg_Close) [ UI.width UI.fill ])
+  let
+    open = Just args.id == args.currently_open_id
+  in 
+    Input.button 
+      ( attributes
+        |> (add_if open (UI.below (view_combo_content attributes { alternatives = args.alternatives, view_alternative = args.view_alternative, message = args.message })))
+        |> (add_if open (Events.onClick <| args.message Msg_Close))
+      )
       { onPress = Just (args.message Msg_Open)
       , label = UI.row 
-        (attributes ++ [ UI.spacing 10 ])
+        [ UI.width UI.fill, UI.spacing 10 ]
         [ args.view_alternative args.combo_state
         , UI.el [ Font.family [ fontawesome ], UI.alignRight ] <| UI.text "\u{f0d7} " -- caret-down
         ]
